748b135b8c05d94c1401af88d15c4bf43c7656e7
Jayank Mayukh <jayank.mayukh@sentieo.com>
2021-06-20 22:58:50 +0530

    Refactor

---
+++ snowpack.config.js
@@ -0,0 +1,34 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    mount: {
+        /* ... */
+    },
+    plugins: [
+        /* ... */
+    ],
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        open: 'chrome',
+        port: 3000,
+    },
+    buildOptions: {
+        /* ... */
+    },
+    exclude: [
+        '**/node_modules/**/*',
+        '**/LICENSE',
+        '**/package.json',
+        '**/package-lock.json',
+        '**/README.md',
+        '**/*.config.js',
+    ],
+    optimize: {
+        bundle: true,
+        minify: true,
+    },
+}
---
+++ src/App.js
@@ -0,0 +1,4 @@
+import Game from "./game/Game";
+
+const game =  new Game({ cols: 30, rows: 30, size: 10 });
+game.play()
